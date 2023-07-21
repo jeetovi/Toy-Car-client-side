ec94332d623ca02434e370c00923539dc4aab0ef
AVIJIT BISWAS <112837308+AVIJIT10-ops@users.noreply.github.com>
2023-07-21 14:16:39 +0600

    install react nap

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,10 @@
 import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../../firebase/firebaseConfig";
+
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app)
 const AuthProvider = ({children}) => {
   const user = { displayName: "Avijit" };
   return <AuthContext.Provider value={user}>
